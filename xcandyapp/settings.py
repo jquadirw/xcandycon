c05d6c11a7b8ec1afeba4dc6e4e6a297ccfdc333
AnyValet <axv@AVPro.lan>
2020-06-26 23:26:09 -0500

    updates

--- xcandyapp/settings.py
+++ xcandyapp/settings.py
@@ -103,6 +103,7 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 # }
 
 # PostgreSQL (heroku)
+SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
 DATABASES = {
     'default': dj_database_url.config(
