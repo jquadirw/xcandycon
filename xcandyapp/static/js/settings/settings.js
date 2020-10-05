28fabc59de478f7a3fa19204ec9961c1ba5d096f
AnyValet <axv@AVPro.local>
2020-10-04 23:28:19 -0500

    preferences form changes

--- xcandyapp/static/js/settings/settings.js
+++ xcandyapp/static/js/settings/settings.js
@@ -1,6 +1,5 @@
 $('#prefsForm').submit(function (e) {
     $.post('/settings/preferences/', $(this).serialize(), function (data) {
-        alert('msg = ' + data.message);
         $('.message').html(data.message);
     });
     e.preventDefault();
